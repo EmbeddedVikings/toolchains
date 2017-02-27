7fa99c155f34cdcc9379a48eaa94cb396433a6d7
Wojciech Jasko <wojciech.jasko@nordicsemi.no>
2017-02-27 09:03:12 +0100

    update toolchain and typo in boards

--- arm-none-eabi.cmake
+++ arm-none-eabi.cmake
@@ -20,7 +20,7 @@ set(CMAKE_SKIP_INSTALL_RULES    ON)
 set(CMAKE_C_FLAGS           "-Wall -fno-builtin -fdata-sections -ffunction-sections -std=gnu99" CACHE INTERNAL "c compiler flags")
 set(CMAKE_CXX_FLAGS         "-Wall -fno-builtin -fdata-sections -ffunction-sections"            CACHE INTERNAL "cxx compiler flags")
 set(CMAKE_ASM_FLAGS         "-x assembler-with-cpp"                                             CACHE INTERNAL "asm compiler flags")
-set(CMAKE_EXE_LINKER_FLAGS  "--specs=nosys.specs -lnosys -Wl,--gc-sections"                     CACHE INTERNAL "exe link flags")
+set(CMAKE_EXE_LINKER_FLAGS  "-lnosys -Wl,--gc-sections"                      CACHE INTERNAL "exe link flags")
 
 SET(CMAKE_C_FLAGS_DEBUG     "-O0 -g -ggdb3" CACHE INTERNAL "c debug compiler flags")
 SET(CMAKE_CXX_FLAGS_DEBUG   "-O0 -g -ggdb3" CACHE INTERNAL "cxx debug compiler flags")
